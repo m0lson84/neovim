3d4d16e3bc01ffb958f2c2f6a083ee5155eda638
Michael Olson <michaelolson84@gmail.com>
2025-03-21 09:56:30 -0400

    Remove deprecated API in grug-far.nvim

--- lua/plugins/editor/grug-far.lua
+++ lua/plugins/editor/grug-far.lua
@@ -2,27 +2,35 @@
 grug-far.nvim
 --]]
 
+--- Search and replace
+local search_and_replace = function()
+  local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
+  require('grug-far').open({
+    prefills = {
+      filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
+    },
+  })
+end
+
+--- Search within the current file
+local search_in_file = function()
+  require('grug-far').open({
+    prefills = {
+      paths = vim.fn.expand('%'),
+    },
+  })
+end
+
 return {
   {
     'MagicDuck/grug-far.nvim',
-    opts = { headerMaxWidth = 80 },
-    cmd = 'GrugFar',
+    opts = {
+      engine = 'ripgrep',
+      transient = true,
+    },
     keys = {
-      {
-        '<leader>sr',
-        function()
-          local grug = require('grug-far')
-          local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
-          grug.grug_far({
-            transient = true,
-            prefills = {
-              filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
-            },
-          })
-        end,
-        mode = { 'n', 'v' },
-        desc = '[s]earch and [r]eplace',
-      },
+      { '<leader>sr', search_and_replace, mode = { 'n', 'v' }, desc = '[s]earch and [r]eplace' },
+      { '<leader>sR', search_in_file, mode = { 'n', 'v' }, desc = '[s]earch / [R]eplace in file' },
     },
   },
 }
