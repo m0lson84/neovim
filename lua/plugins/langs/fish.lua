f7455bcb6caf016f318640f2b52c6c8c7ede4f53
Michael Olson <michaelolson84@gmail.com>
2025-03-25 09:17:11 -0400

    Support fish language

---
+++ lua/plugins/langs/fish.lua
@@ -0,0 +1,52 @@
+--[[
+Fish language support
+--]]
+
+return {
+  -- Add languages to treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts) vim.list_extend(opts.ensure_installed or {}, { 'fish' }) end,
+  },
+
+  -- Configure language server
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        fish_lsp = {},
+      },
+    },
+  },
+
+  -- Configure linters
+  {
+    'mfussenegger/nvim-lint',
+    opts = {
+      linters_by_ft = {
+        fish = { 'fish' },
+      },
+    },
+  },
+
+  -- Configure formatters
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        fish = { 'fish_indent' },
+      },
+    },
+  },
+
+  -- Filetype icons
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      extension = {
+        fish = { glyph = '', hl = 'MiniIconsGrey' },
+        ['fish.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+      },
+    },
+  },
+}
