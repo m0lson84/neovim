0d4e3cec6895cebf79d0fea7dc67ef323771e6f8
Michael Olson <michaelolson84@gmail.com>
2025-08-02 08:30:02 -0400

    Switching to CSpell LSP

--- lua/plugins/linters/cspell.lua
+++ lua/plugins/linters/cspell.lua
@@ -6,26 +6,15 @@ return {
   {
     'mason-org/mason.nvim',
     opts = {
-      ensure_installed = { 'cspell' },
+      ensure_installed = { 'cspell-lsp' },
     },
   },
   {
-    'mfussenegger/nvim-lint',
+    'neovim/nvim-lspconfig',
     dependencies = { 'mason-org/mason.nvim' },
     opts = {
-      linters = {
-        cspell = {
-          condition = function(ctx)
-            local ignored = { 'snacks_picker_input', 'snacks_picker_list' }
-            for _, ft in ipairs(ignored) do
-              if ft == ctx.filetype then return false end
-            end
-            return true
-          end,
-        },
-      },
-      linters_by_ft = {
-        ['*'] = { 'cspell' },
+      servers = {
+        cspell_lsp = {},
       },
     },
   },
