b7b3e82fe32221a7a461c876e3fbc171f7986699
Michael Olson <michael.olson@mutuallyhuman.com>
2025-08-12 11:41:28 -0400

    Use opencode as AI assistant

---
+++ lua/plugins/ai/opencode.lua
@@ -0,0 +1,39 @@
+--[[
+opencode.nvim (https://github.com/NickvanDyke/opencode.nvim)
+--]]
+
+--- Input a prompt to send to opencode
+---@param default? string Text to prefill the input with.
+local input_prompt = function(default)
+  return function() require('opencode').ask(default) end
+end
+
+--- Create a new session
+local new_session = function()
+  return function() require('opencode').command('session_new') end
+end
+
+--- Select prompt to send to opencode
+local select_prompt = function()
+  return function() require('opencode').select_prompt() end
+end
+
+--- Toggle embedded opencode TUI
+local toggle_panel = function()
+  return function() require('opencode').toggle() end
+end
+
+return {
+  {
+    'NickvanDyke/opencode.nvim',
+    dependencies = { 'folke/snacks.nvim' },
+    opts = {},
+    keys = {
+      { '<leader>aa', input_prompt(), mode = { 'n' }, desc = '[a]sk opencode' },
+      { '<leader>aa', input_prompt('@selection'), mode = { 'v' }, desc = '[a]sk selection' },
+      { '<leader>ap', select_prompt(), mode = { 'n', 'v' }, desc = 'select [p]rompt' },
+      { '<leader>as', new_session(), desc = 'new [s]ession' },
+      { '<leader>at', toggle_panel(), desc = '[t]oggle opencode' },
+    },
+  },
+}
