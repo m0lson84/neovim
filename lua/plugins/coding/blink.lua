483868469e962e4354f4397f670679d1be8e059d
Michael Olson <michael.olson@mutuallyhuman.com>
2025-10-14 11:43:29 -0400

    Change layout of completions menu

--- lua/plugins/coding/blink.lua
+++ lua/plugins/coding/blink.lua
@@ -26,10 +26,6 @@ return {
           border = 'rounded',
           draw = {
             treesitter = { 'lsp' },
-            columns = {
-              { 'label', 'label_description', gap = 1 },
-              { 'kind_icon', 'kind', gap = 1 },
-            },
           },
         },
         documentation = {
