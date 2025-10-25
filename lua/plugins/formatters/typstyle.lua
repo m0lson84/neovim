74fdc2e9fe1ecaf81a4c6ee749eb9e8d24475f74
Michael Olson <michaelolson84@gmail.com>
2025-10-25 11:57:53 -0400

    Fix typstyle configuration

--- lua/plugins/formatters/typstyle.lua
+++ lua/plugins/formatters/typstyle.lua
@@ -14,9 +14,9 @@ return {
     opts = {
       formatters = {
         typstyle = {
-          args = function(_, ctx)
+          prepend_args = function(_, ctx)
             local line_length = vim.bo[ctx.buf].textwidth or 120
-            return { '--wrap-text', '-l', tostring(line_length), ctx.filename }
+            return { '-l', tostring(line_length), '--wrap-text' }
           end,
         },
       },
