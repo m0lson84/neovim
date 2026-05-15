e5426952acd2d749267c4d4c987a3fb9b41f194a
Michael Olson <michaelolson84@gmail.com>
2026-05-15 18:02:55 -0400

    Reorganize core options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,84 +1,78 @@
 --[[
-Core Neovim Options
+Core Neovim options
 --]]
 
--- [[ Global variables ]]
+-- [[ Global ]]
 
--- Set <space> as the leader key
--- See `:help mapleader`
--- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
-
--- Providers
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.node_host_prog = vim.fn.expand('~/.local/share/mise/installs/node/lts/bin/neovim-node-host')
 vim.g.python3_host_prog = vim.fn.expand('~/.local/share/mise/installs/python/latest/bin/python')
 
--- [[ Local options ]]
+-- [[ Editor ]]
 
--- Use system clipboard
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
--- Default shell
-vim.o.shell = (vim.fn.executable('fish') and 'fish') or vim.env.SHELL or 'sh'
-
-vim.o.autowrite = true -- Enable auto write
-vim.o.breakindent = true -- Enable break indent
+vim.o.autowrite = true
+vim.o.breakindent = true
 vim.o.completeopt = 'menu,menuone,noselect'
-vim.o.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-vim.o.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.o.cursorline = true -- Highlight current line
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.exrc = true -- Enable project level configuration
-vim.o.grepformat = '%f:%l:%c:%m'
-vim.o.grepprg = 'rg --vimgrep'
-vim.o.ignorecase = true -- Ignore case
-vim.o.inccommand = 'nosplit' -- Preview incremental substitute
+vim.o.confirm = true
+vim.o.expandtab = true
+vim.o.exrc = true
+vim.o.formatoptions = 'jcroqlnt'
 vim.o.jumpoptions = 'view'
-vim.o.laststatus = 3 -- Global statusline
-vim.o.linebreak = true -- Wrap lines at convenient points
-vim.o.list = true -- Show some invisible characters (tabs...)
-vim.o.mouse = 'a' -- Enable mouse mode
-vim.o.number = true -- Print line number
-vim.o.pumblend = 10 -- Popup blend
-vim.o.pumheight = 10 -- Maximum number of entries in a popup
-vim.o.relativenumber = true -- Relative line numbers
-vim.o.scrolloff = 4 -- Lines of context
+vim.o.mouse = 'a'
 vim.o.sessionoptions = 'buffers,curdir,tabpages,winsize,help,globals,skiprtp,folds'
-vim.o.shiftround = true -- Round indent
-vim.o.shiftwidth = 2 -- Size of an indent
-vim.o.cmdheight = 0 -- Hide cmdline when not in use
-vim.o.showmode = false -- Don't show mode since we have a statusline
-vim.o.sidescrolloff = 8 -- Columns of context
-vim.o.signcolumn = 'yes' -- Always show the signcolumn
-vim.o.smartcase = true -- Don't ignore case with capitals
-vim.o.smartindent = true -- Insert indents automatically
+vim.o.shell = (vim.fn.executable('fish') and 'fish') or vim.env.SHELL or 'sh'
+vim.o.shiftround = true
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
 vim.o.spelllang = 'en'
-vim.o.splitbelow = true -- Put new windows below current
-vim.o.splitkeep = 'screen'
-vim.o.splitright = true -- Put new windows right of current
-vim.o.tabstop = 2 -- Number of spaces tabs count for
-vim.o.timeoutlen = 300 -- Lower than default to quickly trigger which-key
-vim.o.undofile = true -- Save undo history
+vim.o.tabstop = 2
+vim.o.timeoutlen = 300
+vim.o.undofile = true
 vim.o.undolevels = 10000
-vim.o.updatetime = 200 -- Save swap file and trigger CursorHold
-vim.o.virtualedit = 'block' -- Allow cursor to move where there is no text in visual block mode
-vim.o.wildmode = 'longest:full,full' -- Command-line completion mode
-vim.o.winminwidth = 5 -- Minimum window width
-vim.o.wrap = false -- Disable line wrap
-
+vim.o.updatetime = 200
+vim.o.virtualedit = 'block'
+vim.o.wildmode = 'longest:full,full'
 vim.opt.shortmess:append('WcC')
 vim.opt.spelloptions:append('noplainbuffer')
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.fillchars = {
-  diff = '╱',
-  eob = ' ',
-}
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
--- Formatter
-vim.o.formatoptions = 'jcroqlnt'
+-- [[ Search ]]
+
+vim.o.grepformat = '%f:%l:%c:%m'
+vim.o.grepprg = 'rg --vimgrep'
+vim.o.ignorecase = true
+vim.o.inccommand = 'nosplit'
+vim.o.smartcase = true
+
+-- [[ UI ]]
+
+vim.o.cmdheight = 0
+vim.o.conceallevel = 2
+vim.o.cursorline = true
+vim.o.laststatus = 3
+vim.o.linebreak = true
+vim.o.list = true
+vim.o.number = true
+vim.o.pumblend = 10
+vim.o.pumheight = 10
+vim.o.relativenumber = true
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.wrap = false
+vim.opt.fillchars = { diff = '╱', eob = ' ' }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- [[ Windows ]]
+
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 8
+vim.o.splitbelow = true
+vim.o.splitkeep = 'screen'
+vim.o.splitright = true
+vim.o.winminwidth = 5
 
 -- [[ File types ]]
 
