1bd3e38bed3737d9b12821dd083f8f7434a1f697
Michael Olson <michaelolson84@gmail.com>
2026-06-29 10:36:46 -0400

    Fix lsp config overrides

--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -6,12 +6,12 @@ LSP utilities
 local M = {}
 
 --- Get all configured LSP server names.
---- Scans `lsp/*.lua` files in the user config directory (`stdpath('config')`)
+--- Scans `after/lsp/*.lua` files in the user config directory (`stdpath('config')`)
 --- and returns server names derived from the filenames (without the `.lua` extension).
 ---@return string[] servers List of LSP server names.
 function M.servers()
   local servers = {}
-  for _, path in ipairs(vim.fn.glob(vim.fn.stdpath('config') .. '/lsp/*.lua', false, true)) do
+  for _, path in ipairs(vim.fn.glob(vim.fn.stdpath('config') .. '/after/lsp/*.lua', false, true)) do
     table.insert(servers, vim.fn.fnamemodify(path, ':t:r'))
   end
   return servers
